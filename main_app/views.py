6e63ec498723b43ad646cdaf0d7c58dc8fcda62c
Joseph Choi <jchoi192@macbook-pro.myfiosgateway.com>
2021-07-24 16:47:23 -0400

    removes pycache files

--- main_app/views.py
+++ main_app/views.py
@@ -60,7 +60,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('home')
         else:
             error_message = 'Invalid Signup Data - Please Try Again'
 
